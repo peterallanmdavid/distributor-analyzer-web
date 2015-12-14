7a474b48fa53f533013cdc2d934e6ffcfb97fee0
Peter Allan David <Peter Allan David>
2015-12-15 00:01:58 +0800

    update all

--- react/js/components/presentation/presentation.list.item.react.js
+++ react/js/components/presentation/presentation.list.item.react.js
@@ -5,6 +5,7 @@ var React = require('react')
     , ClientList = require('../manage/clients/manage.clients.list.react')
     , VehicleList = require('../manage/vehicle/manage.vehicle.list.react')
     , ActionButtons = require('./presentation.list.actions.react')
+    , classnames = require('classnames')
     ;
 
 var PresentationListItem = React.createClass({
@@ -75,30 +76,54 @@ var PresentationListItem = React.createClass({
     _closeModal:function(){
         this.setState({showModal:false})
     },
+
+    _readMore:function(text,title){
+        this.setState({
+            showModal: true,
+            confirmMethod: function(){},
+            confirmParams: {},
+            bodyText:text,
+            title:title
+
+        })
+    },
     render: function () {
         var d = this.props.presentationDataItem;
+        var ciClass = classnames("readmore-btn pointer", {"hidden": (typeof d.completedInvestigation==="undefined" || d.completedInvestigation==="")});
+        var currIClass = classnames("readmore-btn pointer", {"hidden":  (typeof d.currentIntelligence==="undefined" || d.currentIntelligence==="")});
+        var plClass = classnames("readmore-btn pointer", {"hidden": (typeof d.pendingLeads==="undefined" || d.pendingLeads==="")});
+        var tlCLass = classnames("readmore-btn pointer", {"hidden": (typeof d.taskingLeads==="undefined" || d.taskingLeads==="")});
+
+
+        var vehicles = d.vehicles.length>0?<GenericButton buttonText ="VIEW DETAILS" onClickHandler = {this._viewVehiclesDetails}/>:<span></span>;
+        var clients = d.clients.length>0?<GenericButton buttonText ="VIEW DETAILS" onClickHandler = {this._viewClientDetails}/>:<span></span>
+        var sources = d.sources.length>0?<GenericButton buttonText ="VIEW DETAILS" onClickHandler = {this._viewSourcesDetails}/>:<span></span>
+
         return (
 
                 <div className = "table-row">
                     <div className = "table-col"><ActionButtons id = {d.id.toString()}/></div>
                     <div className = "table-col">{d.name}</div>
                     <div className = "table-col">{d.location}</div>
-                    <div className = "table-col">{d.vehicles.length} <GenericButton
-                        buttonText ="view details"
-                        onClickHandler = {this._viewVehiclesDetails}
-                    /></div>
-                    <div className = "table-col">{d.sources.length} <GenericButton
-                        buttonText ="view details"
-                        onClickHandler = {this._viewSourcesDetails}
-                    /></div>
-                    <div className = "table-col">{d.clients.length} <GenericButton
-                        buttonText ="view details"
-                        onClickHandler = {this._viewClientDetails}
-                    /></div>
-                    <div className = "table-col long-text">{d.completedInvestigation}</div>
-                    <div className = "table-col long-text">{d.currentIntelligence}</div>
-                    <div className = "table-col long-text">{d.pendingLeads}</div>
-                    <div className = "table-col long-text end">{d.taskingLeads}</div>
+                    <div className = "table-col with-details" >{d.vehicles.length} {vehicles} </div>
+                    <div className = "table-col with-details">{d.sources.length} {sources} </div>
+                    <div className = "table-col with-details">{d.clients.length} {clients} </div>
+                    <div className = "table-col long-text">
+                        <span className = "ellipsis">{d.completedInvestigation}</span>
+                        <span className = {ciClass} onClick = {this._readMore.bind(null, d.completedInvestigation, "Completed Investigation")}>READ MORE</span>
+                    </div>
+                    <div className = "table-col long-text">
+                        <span className = "ellipsis">{d.currentIntelligence}</span>
+                        <span className = {currIClass} onClick = {this._readMore.bind(null, d.currentIntelligence, "Current Intelligence")}>READ MORE</span>
+                    </div>
+                    <div className = "table-col long-text">
+                        <span className = "ellipsis">{d.pendingLeads}</span>
+                        <span className = {plClass} onClick = {this._readMore.bind(null, d.pendingLeads, "Pending Leads")}>READ MORE</span>
+                    </div>
+                    <div className = "table-col long-text end">
+                        <span className = "ellipsis">{d.taskingLeads}</span>
+                        <span className = {tlCLass} onClick = {this._readMore.bind(null, d.taskingLeads, "Tasking Leads")}>READ MORE</span>
+                    </div>
                     <div>
                         <Modal
                             showModal={this.state.showModal}
